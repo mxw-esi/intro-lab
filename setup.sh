b9d8a0a487da31f7cdeab78c6701a920f28f9260
Mike <mbne08@gmail.com>
2022-06-04 22:46:35 -0500

    commit the new functions

--- setup.sh
+++ setup.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
 
-file1=/tmp/hidden/.folder/flag.txt
+############################################################
+# Created by Michael Woodside for the Intro to Linux Class #
+#                                                          #
+#  This script will do several different things,           #
+#  from creating some random Folder Structures,            #
+#  hiding files in the random locations,                   #
+#  to setting up a new root user for the instructor        #
+#  and having the users in the class find the new user     #
+#  and then remove that user                               #
+#                                                          #
+############################################################
 
-mkdir -p /tmp/hidden/.folder/
-touch $file1
+# Creating variables for the script
+file1=flag.txt
+folder1=$RANDOM
+folder2=$RANDOM 
+logfile=/var/log/class.log
 
 
-cat <<EOF > $file1
+# Functions for the script 
+
+#this simpe function creates a random file each time the command is run, it uses a new random #
+Create_files() {
+
+mkdir -p /tmp/hidden/.$folder1/$folder2/
+touch /tmp/hidden/.$folder1/$folder2/$file1
+touch $logfile
+echo /tmp/hidden/.$folder1/$folder2/$file1 >> $logfile
+
+
+cat <<EOF > /tmp/hidden/.$folder1/$folder2/$file1
 This is is a file
 in a hidden directory
 {{flag}}
 EOF
+}
+
+create_instructor () {
+
+    useradd -G root,adm,sudo -l -u 777 -m -d /home/.1/teach instructor1
+    id instructor1 >> $logfile
+    echo "instructor1:1qaz2wsx!QAZ@WSX" | chpasswd
+    dircolors -p > /home/.1/teach/.dircolors
+        
+}
+
+#Executing the actual commands for the Rest of the Script.
+Create_files
+create_instructor
+
