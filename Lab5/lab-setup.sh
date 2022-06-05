c6e54f76bcd091456261a24ffe4e928a9cc7077f
Mike <mbne08@gmail.com>
2022-06-05 14:42:45 -0500

    making directorys and hiding the script

--- Lab5/lab-setup.sh
+++ Lab5/lab-setup.sh
@@ -15,11 +15,16 @@
 # Creating variables for the script
 file1=flag.txt
 folder1=$RANDOM
+folder2=$RANDOM
 logfile=/var/log/lab5.log
 
+mkdir -p /home/labuser*/$folder1
 touch /home/labuser*/$folder1/$file1
 
-cat <<EOF > /tmp/hidden/.$folder1/$folder2/$file1
+read -p 'Please enter a number between 5-25: ' depthvar
+
+# creating a basic file with some text in it.
+cat <<EOF > /home/labuser*/$folder1/$file1
 This is is a file that was created using the script
 {{flag}}
 
@@ -36,3 +41,29 @@ touch /home/labuser*/$folder1/$file1
 
 
 EOF
+
+echo 'Now its time to find a needle in a haystack'
+echo 'Based on the question asked when you started this script there is a file with the name flag in it'
+echo 'it is burried dep, but how deep'
+
+
+new_files () {
+cd /tmp
+for((i=$1;i>=0;--i)) do
+    num=$RANDOM
+    mkdir $num
+    cd $num
+done
+
+    
+}
+
+hide_me () {
+    
+    mv $PWD/lab-setup.sh $PWD/.lab-setup.sh
+}
+
+
+hide_me
+new_files $depthvar
+
