1799a4053ecfd5cd84b196e46bdcb4cd00ca23f9
Michael Woodside <Michael.Woodside@expresspros.com>
2022-06-06 14:55:36 -0500

    history commands removed

--- Lab5/runme1.sh
+++ Lab5/runme1.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
-############################################################
-# Created by Michael Woodside for the Intro to Linux Class #
-#                                                          #
-#  This script will run a few random commands to fill      #
-#  the history buffer up so that students have some output.#
-#                                                          #
-############################################################
+#############################################################
+# Created by Michael Woodside for the Intro to Linux Class  #
+#                                                           #
+#  This script will run a few random commands but they wont #
+#  fill the history buffer up, so that students wont have   #
+#  any script history.                                      #
+#                                                           #
+#############################################################
 
 # Creating variables for the script
 
 comvar=$(($RANDOM % 30 + 4))
 echo $comvar
-set -o history
+
 
 for((i=$comvar;i>=0;--i)) do
-    history -s whoami > /dev/null 2>&1
-    history -s date > /dev/null 2>&1
-    history -s cal > /dev/null 2>&1
-    history -s shazam > /dev/null 2>&1
-    history -s sdo > /dev/null 2>&1
-    history -s fishing > /dev/null 2>&1
-    history -s whatever > /dev/null 2>&1
-    history -s tranquil > /dev/null 2>&1
+    whoami > /dev/null 2>&1
+    date > /dev/null 2>&1
+    cal > /dev/null 2>&1
+    shazam > /dev/null 2>&1
+    sdo > /dev/null 2>&1
+    fishing > /dev/null 2>&1
+    whatever > /dev/null 2>&1
+    tranquil > /dev/null 2>&1
 
 done
