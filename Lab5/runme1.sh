4ea557359f4847710646baa7d5a23f2022844048
Michael Woodside <Michael.Woodside@expresspros.com>
2022-06-06 14:48:10 -0500

    new history script

---
+++ Lab5/runme1.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+############################################################
+# Created by Michael Woodside for the Intro to Linux Class #
+#                                                          #
+#  This script will run a few random commands to fill      #
+#  the history buffer up so that students have some output.#
+#                                                          #
+############################################################
+
+# Creating variables for the script
+
+comvar=$(($RANDOM % 30 + 4))
+echo $comvar
+set -o history
+
+for((i=$comvar;i>=0;--i)) do
+    history -s whoami > /dev/null 2>&1
+    history -s date > /dev/null 2>&1
+    history -s cal > /dev/null 2>&1
+    history -s shazam > /dev/null 2>&1
+    history -s sdo > /dev/null 2>&1
+    history -s fishing > /dev/null 2>&1
+    history -s whatever > /dev/null 2>&1
+    history -s tranquil > /dev/null 2>&1
+
+done
