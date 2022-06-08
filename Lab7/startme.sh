7ed77862edfa697b3626f1a073f46b5f9fd2d58b
Michael Woodside <Michael.Woodside@expresspros.com>
2022-06-08 15:12:58 -0500

    trying to move stuff

--- Lab7/startme.sh
+++ Lab7/startme.sh
@@ -4,5 +4,5 @@ apt install -y shc gcc
 cd /tmp
 git clone https://github.com/mxw-esi/intro-lab.git
 
-chmod +x /tmp/intro-lab/Lab7/lab-setup.x
-/tmp/intro-lab/Lab7/lab-setup.x
+chmod +x /tmp/intro-lab/Lab7a/lab-setup.sh
+/tmp/intro-lab/Lab7a/lab-setup.sh
---
+++ Lab7a/lab-setup.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+############################################################
+# Created by Michael Woodside for the Intro to Linux Class #
+#                                                          #
+#  This script will do several different things,           #
+#  from creating some random Folder Structures,            #
+#  hiding files in the random locations.                   #
+#                                                          #
+#                                                          #
+############################################################
+
+#if [ $1 -ne 1 ] 
+#then 
+#echo "Please specify how many random words would you like to generate !" 1>&2 
+#echo "example: ./random-word-generator 3" 1>&2 
+#echo "This will generate 3 random words" 1>&2 
+#exit 0
+#fi 
+
+
+# Creating variables for the script
+file1=flag.txt
+folder1=$RANDOM
+folder2=$RANDOM
+logfile=/var/log/lab5.log
+scriptdir=/home/labuser*/intro-lab/Lab7/
+
+
+touch $logfile
+cd /home/labuser*
+mkdir $PWD/$folder1
+touch $PWD/$folder1/$file1
+
+read -p 'Please enter a number between 5-25, if you are Max please enter 1: ' depthvar
+
+# creating a basic file with some text in it.
+
+cat <<EOF > /home/labuser*/$folder1/$file1
+This is is a file that was created using the script
+{{flag}}
+
+
+
+here is a copy of the script
+
+
+file1=flag.txt
+folder1=$RANDOM
+logfile=/var/log/lab5.log
+
+touch /home/labuser*/$folder1/$file1
+
+
+EOF
+
+echo 'Now its time to find a needle in a haystack'
+echo 'Based on the question asked when you started this script there is a file with the name flag in it'
+echo 'it is buried deep, but how deep'
+
+
+new_files () {
+cd /tmp
+for((i=$1;i>=0;--i)) do
+    num=$RANDOM
+    mkdir $num
+    cd $num
+done
+touch $PWD/findersflagkeeper.txt
+files
+}
+
+files() {
+cat <<EOF > $PWD/findersflagkeeper.txt
+This is is a file that was created using the script
+{{flag}}
+
+Welcome to the class $USER
+
+
+EOF
+
+}
+
+challenge_me () {
+cd /home/labuser*
+touch .bash_alias
+echo 'alias ls='find >> /home/labuser*/.bash_alias
+echo 'alias cat='tac >> /home/labuser*/.bash_alias
+echo 'alias man='\'echo\ \'this\\ might\\ be\\ broken\'\' >> /home/labuser*/.bash_alias
+echo 'alias grep='\'sudo init\ 6\' >> /home/labuser*/.bash_alias
+echo 'alias nano='vi >> /home/labuser*/.bash_alias
+echo '. /home/labuser5/.bash_alias' >> /home/labuser*/.bashrc
+echo 'look at your bashrc file, if you can find it' >> $logfile
+
+}
+
+hide_me () {
+
+    cd $scriptdir
+    mv $PWD/lab-setup.sh $PWD/.lab-setup.sh
+}
+
+if [ $depthvar -eq 1 ];then
+
+
+new_files 32
+challenge_me
+hide_me
+init 6
+
+else
+new_files $depthvar
+hide_me
+fi
